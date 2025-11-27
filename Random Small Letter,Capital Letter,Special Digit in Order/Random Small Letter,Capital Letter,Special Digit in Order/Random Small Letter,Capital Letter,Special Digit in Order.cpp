60651d71ab27220650a8b18c149c46fe3e9e76ba
m0hamedgamal2oo4 <mohamedwezo2004@gmail.com>
2025-11-27 17:01:17 +0200

    Finished problem X and fixed bug in problem Y

---
+++ Random Small Letter,Capital Letter,Special Digit in Order/Random Small Letter,Capital Letter,Special Digit in Order/Random Small Letter,Capital Letter,Special Digit in Order.cpp
@@ -0,0 +1,60 @@
+// Random Small Letter,Capital Letter,Special Digit in Order.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>  
+#include <string>    
+#include <cstdlib>   
+#include <ctime>   
+using namespace std;
+
+enum enCharType{SmallLetter=1,CapitalLetter=2,SpecailCaracter=3,Digit=4};
+
+int RandomNumber(int From, int To) {
+	int randomNum = rand() % (To - From + 1) + From;
+	return randomNum;
+}
+
+
+
+
+char GetRandomCharacter(enCharType CharType)
+{
+	switch (CharType)
+	{ 
+
+	case enCharType::SmallLetter: {
+          return char(RandomNumber(97, 122));
+			break;}
+	case enCharType::CapitalLetter:
+	{
+		return char(RandomNumber(65, 90));
+		break;
+	}
+
+	case enCharType::SpecailCaracter:
+	{
+		return char(RandomNumber(33, 47));
+		break;
+	}
+	case enCharType::Digit:
+	{
+		return char(RandomNumber(48, 57));
+	}
+	
+	}
+		
+			
+}
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	cout << GetRandomCharacter(enCharType::SmallLetter) << endl;
+	cout << GetRandomCharacter(enCharType::CapitalLetter) << endl;
+	cout << GetRandomCharacter(enCharType::SpecailCaracter) << endl;
+	cout << GetRandomCharacter(enCharType::Digit) << endl;
+
+
+}
